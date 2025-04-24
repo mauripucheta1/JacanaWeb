9e256b2d0e1b0ed0288cce2666d2190c684fe40c
Mauricio Pucheta <mauriciopucheta21@gmail.com>
2025-04-24 15:28:45 -0300

    fix: Corrección en los puertos para permitir usar el local.

--- BackEnd/server/server.js
+++ BackEnd/server/server.js
@@ -21,7 +21,7 @@ app.use('/clientes', clientesRoutes);
 app.use('/auth', authRoute)
 
 // Servidor
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
