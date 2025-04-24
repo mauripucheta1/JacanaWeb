48bc23fb4ac5d957ec9bcac24275587f0a71bd50
Mauricio Pucheta <mauriciopucheta21@gmail.com>
2025-04-24 14:20:10 -0300

    update: Actualización en astro.config.mjs. Se actualizó el output de tal manera que sea estático y se elimino el archivo 'tsconfig.json.

--- FrontEnd/astro.config.mjs
+++ FrontEnd/astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  output: 'static',
   integrations: [tailwind()],
   vite: {
     server: {
       fs: {
         allow: [
-          './',               // Permitir el acceso al directorio raíz del frontend
-          '../node_modules',  // Permitir acceso a las dependencias instaladas
-          '../BackEnd',       // Si alguna referencia al backend es necesaria (opcional)
+          './',               
+          '../node_modules',  
+          '../BackEnd',       
         ],
       },
     },
