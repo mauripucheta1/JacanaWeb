1f726e2ee16cda7a786276db7fd4ab0596c99eda
Mauricio Pucheta <mauriciopucheta21@gmail.com>
2025-04-24 17:46:46 -0300

    fix: Corrección de 'app' a 'router'.

--- BackEnd/server/routes/auth.js
+++ BackEnd/server/routes/auth.js
@@ -3,11 +3,12 @@ const { login } = require('../controllers/authController');
 
 const router = express.Router();
 
-// Ruta madre
-app.get('/', (req, res) => {
+// Ruta raíz
+router.get('/', (req, res) => {
     res.send('¡Servidor Backend funcionando correctamente!');
 });
 
+// Ruta login
 router.post('/login', login); 
 
 module.exports = router;
