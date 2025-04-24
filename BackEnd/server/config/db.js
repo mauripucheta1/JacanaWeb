e266df155d0a3c169d57b866718d715e200e952c
Mauricio Pucheta <mauriciopucheta21@gmail.com>
2025-04-24 15:50:44 -0300

    update: Actualización en archivo 'config/db.js'. Se actualizó el uso de variables de entorno.

--- BackEnd/server/config/db.js
+++ BackEnd/server/config/db.js
@@ -2,24 +2,25 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT, // Usar el puerto 5432
+  ssl: {
+    rejectUnauthorized: false, // Necesario para Railway
+  },
 });
 
-console.log('Variables de entorno:', process.env);
-
 const testDBConnection = async () => {
-    try {
-        const result = await pool.query('SELECT * FROM administradores');
-        console.log('Contenido de la tabla administradores:', result.rows);
-    } catch (error) {
-        console.error('Error al conectarse a la base de datos:', error);
-    }
+  try {
+    const result = await pool.query('SELECT 1 + 1 AS result');
+    console.log('Conexión exitosa. Resultado:', result.rows);
+  } catch (error) {
+    console.error('Error al conectarse a la base de datos:', error);
+  }
 };
 
-testDBConnection(); 
+testDBConnection();
 
 module.exports = pool;
