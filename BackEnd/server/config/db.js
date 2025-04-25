a966c5c1244083083a3d1dcb83ddf91036010763
Mauricio Pucheta <mauriciopucheta21@gmail.com>
2025-04-24 22:39:07 -0300

    delete: Se eliminaron console.log innecesarios del archivo 'db.js'.

--- BackEnd/server/config/db.js
+++ BackEnd/server/config/db.js
@@ -12,11 +12,6 @@ const pool = new Pool({
   },
 });
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 const testDBConnection = async () => {
   try {
     const result = await pool.query('SELECT 1 + 1 AS result');
